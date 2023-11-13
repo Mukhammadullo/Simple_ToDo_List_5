3f7d64dd75d98b4ff9d35eed450d8b169b33ab3e
Muhammadullo <mnastulloev@gmail.com>
2023-11-13 08:24:40 +0500

    title

--- main.js
+++ main.js
@@ -159,3 +159,18 @@ btnAdd.onclick = () => {
     inpAddName.value = ""
     inpAddCity.value = ""
 }
+
+
+
+
+// recursion function
+// function factorial(n) {
+//     if (n <= 1) {
+//         return 1;
+//     } else {
+//         return n * factorial(n - 1);
+//     }
+// }
+// console.log(factorial(5));
+
+
